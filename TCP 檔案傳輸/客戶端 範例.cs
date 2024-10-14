91adfe4c63dd49f2653ba4c272571119c07f4eb7
Wechli <65846789+alan198978@users.noreply.github.com>
2024-10-14 08:21:03 +0800

    Update 客戶端 範例.cs

--- TCP 檔案傳輸/客戶端 範例.cs
+++ TCP 檔案傳輸/客戶端 範例.cs
@@ -52,7 +52,7 @@ class Client
                     byte[] 檔案資料內容 = new byte[fileSize];
                     int bytesReceived = 0; //每次最初設定為 0，因為在開始接收之前，我們還沒有接收到任何資料。
 
-                    while (bytesReceived < fileSize) //*註解1(請拉到最底下)
+                    while (bytesReceived < fileSize)
                     {
                         bytesReceived += stream.Read(檔案資料內容, bytesReceived, fileSize - bytesReceived);
                     }
@@ -81,10 +81,3 @@ class Client
         Console.ReadLine(); // 留一個空白輸入，避免測試完畢時直接被關閉視窗。
     }
 }
-//註解*1:
-//它使用一個 while 迴圈來確保完整的檔案資料被接收到，特別是在網路傳輸時，資料並不總是一次性全部傳完。
-//bytesReceived < fileSize 迴圈的條件判斷讓迴圈會持續執行，直到接收到的總位元組數量 (bytesReceived) 等於或超過檔案的大小 (fileSize)。
-//接著是 stream.Read(檔案資料內容, bytesReceived, fileSize - bytesReceived) :
-//檔案資料內容: 這是我們之前創建的 byte 陣列，用來儲存接收到的檔案資料。
-//bytesReceived: 表示從陣列的哪個位置開始寫入讀取到的資料。第一次會從 0 開始，隨著接收到更多資料，這個值會增加，以確保新接收到的資料寫入陣列的正確位置。
-//fileSize - bytesReceived: 這是我們這次讀取資料的最大長度，它表示剩餘的檔案資料大小，即還需要接收的位元組數量。這樣可以確保我們不會試圖讀取超過檔案實際大小的資料。
